35d5b3bcfedc1266045368f0fc29b621195ef52b
Yusuke Sangenya <longinus.eva@gmail.com>
2019-04-06 12:24:23 +0900

    Remove magic number (magic string)

--- update.sh
+++ update.sh
@@ -5,10 +5,10 @@ set -ue
 cd "$FEEDS_DIR"
 git pull
 bundle install --path vendor/bundle
-cat ~/my-articles/feeds_me.toml | bundle exec ruby crawl.rb | bundle exec ruby generate.rb > dist/feeds.json
+cat "$ARTICLES_DIR"/feeds_me.toml | bundle exec ruby crawl.rb | bundle exec ruby generate.rb > dist/feeds.json
 
 cd "$ARTICLES_DIR"
 git pull
 PKG_CONFIG_PATH=/usr/lib/imagemagick6/pkgconfig bundle install --path vendor/bundle
-BLOG_FEED_PATH=~/feeds/dist/feeds.json bundle exec ruby site.rb
+BLOG_FEED_PATH="$FEEDS_DIR"/dist/feeds.json bundle exec ruby site.rb
 cp -r _site/* /var/articles
